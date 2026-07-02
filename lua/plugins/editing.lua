6c6a7c327554ed5798f819ffca33c248fffaf02b
Jannik Buhr <jannik.m.buhr@gmail.com>
2026-07-02 10:31:24 +0200

    feat: use default commenting without plugin

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -9,16 +9,16 @@ return {
     opts = {},
   },
 
-  { -- commenting with e.g. `gcc` or `gcip`
-    -- respects TS, so it works in quarto documents 'numToStr/Comment.nvim',
-    'numToStr/Comment.nvim',
-    version = nil,
-    cond = function()
-      return vim.fn.has 'nvim-0.10' == 0
-    end,
-    branch = 'master',
-    config = true,
-  },
+  -- { -- commenting with e.g. `gcc` or `gcip`
+  --   -- respects TS, so it works in quarto documents 'numToStr/Comment.nvim',
+  --   'numToStr/Comment.nvim',
+  --   version = nil,
+  --   cond = function()
+  --     return vim.fn.has 'nvim-0.10' == 0
+  --   end,
+  --   branch = 'master',
+  --   config = true,
+  -- },
 
   { -- format things as tables
     'godlygeek/tabular',
